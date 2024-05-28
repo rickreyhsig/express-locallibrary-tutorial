4c234cb19276157043fe99a5ed29504b483bd4be
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2024-05-28 16:10:51 -0400

    Add messageBoard

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ricardo\'s Express app' });
+  res.render('index', { title: "Mini Messageboard", messages: messages })
 });
+
+/* GET messages. */
+router.get('/new', function(req, res, next) {
+  res.render('form')
+});
+
+/* POST messages. */
+router.post('/new', function(req, res, next) {
+  const { messageText, messageUser } = req.body; // Extracting values from req.body
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect('/')
+});
+
+
 
 module.exports = router;
